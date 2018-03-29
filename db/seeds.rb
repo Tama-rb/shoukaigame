57494f62d3bf04c1b04008de0c72d9b8b485c673
hoqqun <hoqqun@gmail.com>
2018-03-29 10:40:55 +0900

    修正 戻るリンク

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 conj_list = %w(そして 
                だから
                ちなみに 
