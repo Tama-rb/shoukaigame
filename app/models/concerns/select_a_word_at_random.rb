caa33bb03f78e1dfb37fe1f07a4b19a4a3bd516d
hoqqun <hoqqun@gmail.com>
2018-10-28 19:25:53 +0900

    refactor ランダムメソッドをやめてサンプルメソッドにした

--- app/models/concerns/select_a_word_at_random.rb
+++ app/models/concerns/select_a_word_at_random.rb
@@ -7,16 +7,14 @@ module SelectAWordAtRandom
 
   module ClassMethods
     def select_a_word_at_random
-      record = find(random_id)
+      record = all.sample_other_than_latest
       record.increment_impressions
+
       record
     end
 
-    private
-
-      def random_id
-        random = Random.new
-        random.rand(1..count)
-      end
+    def sample_other_than_latest
+      count > 1 ? first(count - 1).sample : sample
+    end
   end
 end
